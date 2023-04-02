c311870bb1a824b902bec49ad488f5353e925c1a
RealTong <i@wst.sh>
2023-04-02 12:06:13 +0800

    Update font and components for better UX
    - Update data fetching in Activity component
    - Add condition to render either NowPlaying or ContributionGraph
    - Update font files and font face in globals.css
    - Update animation delays in MusicBar component
    - Change font type in LatestPosts component
    
    [components/Activity.jsx]
    - Change the way data is fetched from the API
    - Add a condition to render either the NowPlaying component or the ContributionGraph component
    [styles/globals.css]
    - Add font face for 'LXGW WenKai' font
    - Add class for 'LXGW WenKai' font
    [components/MusicBar.jsx]
    - Changed the animation delays for the MusicBar component
    [public/fonts/LXGWWenKai-Regular.woff]
    - Add a new font file, `LXGWWenKai-Regular.woff`
    [public/fonts/LXGWWenKai-Regular.woff2]
    - Add the `LXGWWenKai-Regular.woff2` font file
    [components/LatestPosts.jsx]
    - Change font type in LatestPosts component from `font-['霞鹜文楷']` to `weiKaiFont`

--- components/Activity.jsx
+++ components/Activity.jsx
@@ -4,14 +4,13 @@ import fetcher from "../lib/fetcher";
 import GetContributionGraph from "./ContributionGraph";
 
 function Activity() {
-    const {data, isLoading} = useSWR("/api/now", fetcher);
-
+    const {data} = useSWR("/api/now", fetcher);
     return (
         <div className={""}>
             <p className={"my-4 text-3xl font-bold"}>Activity</p>
             <div className={"flex justify-center"}>
                 {
-                    data ? <NowPlaying song={data}/> :<GetContributionGraph />
+                    data?.songUrl ? <NowPlaying song={data}/>:<GetContributionGraph/>
                 }
             </div>
         </div>
--- components/MusicBar.jsx
+++ components/MusicBar.jsx
@@ -2,8 +2,8 @@ function MusicBar() {
     return (
         <div className="relative flex justify-between w-6 h-6">
             <span className={"w-1.5 h-full bg-[#1ED760] rounded-xl animate-[health_1.6s_ease_infinite_alternate] origin-bottom"}></span>
-            <span className={"w-1.5 h-full bg-[#1ED760] rounded-xl animate-[health_2.4s_ease_infinite_alternate] origin-bottom delay-[2200ms]"}></span>
-            <span className={"w-1.5 h-full bg-[#1ED760] rounded-xl animate-[health_3.0s_ease_infinite_alternate] origin-bottom delay-[3700ms]"}></span>
+            <span className={"w-1.5 h-full bg-[#1ED760] rounded-xl animate-[health_2.4s_ease_infinite_alternate] origin-bottom"}></span>
+            <span className={"w-1.5 h-full bg-[#1ED760] rounded-xl animate-[health_3.0s_ease_infinite_alternate] origin-bottom"}></span>
         </div>
     )
 }
