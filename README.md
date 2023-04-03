b1ae3ebef4f1486604d7c5c72d66fbf7a6848097
RealTong <i@wst.sh>
2023-04-03 23:39:26 +0800

    Update Footer, Tailwind config, README, and screenshot
    - Add toggle theme button with icon and function to toggle theme
    - Update copyright link, design link, link to personal blog and portfolio, Creative Commons license info
    - Add new framework, CMS, styling and deployment
    - Add screenshot image
    
    [components/Footer.jsx]
    - Add toggle theme button with icon
    - Add function to toggle theme
    - Update copyright link
    - Update design link
    [tailwind.config.js]
    - Add dark mode option to Tailwind configuration
    [README.md]
    - Add new framework, CMS, styling and deployment
    - Add screenshot image
    - Update link to personal blog and portfolio
    - Update Creative Commons license info
    [screen-shot/screenshot.png]
    - Add a new screenshot file

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
+
+# realtong.cn
+
+---
+* Framework: Next.js
+* CMS: Notion
+* Styling: Tailwind CSS
+* Deployment: Vercel
+
+<img src="./screen-shot/screenshot.png">
+
 <samp><a href="https://realtong.cn" target="_blank" rel="noopener noreferrer">realtong.cn</a></samp>
 
 personal blog and portfolio, created by [RealTong](https://github.com/RealTong), Design by [Diu](https://github.com/ddiu8081)
-
-<br>
 
 <samp>contents licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY-NC-SA 4.0</a></samp>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,26 @@
 import Link from "next/link";
+import {CgDarkMode} from "react-icons/cg";
 
-function Footer({align}){
-    return(
-        <>
-            <p className={`mt-10 w-full text-${align}`}>
+function Footer({align}) {
+    const toggleTheme = () => {
+        const html = document.querySelector("html");
+        html.classList.toggle("dark");
+    }
+    return (
+        <div className={"flex mt-10"}>
+            <p className={`w-full text-${align}`}>
                 Copyright <Link href="https://realtong.cn" className={"underline"}>Tong</Link>{` \u00A9 ${getYear()}`}.
                 Design by <Link href="https://ddiu.io/" className={"hover:text-[#789388] underline"}>ddiu.io</Link>
             </p>
-        </>
+            <button className={"text-2xl"} onClick={toggleTheme}>
+                <CgDarkMode/>
+            </button>
+        </div>
     )
 }
+
 function getYear() {
     return new Date().getFullYear();
 }
+
 export default Footer;
