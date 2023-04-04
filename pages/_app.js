96c7cf7193ed72c5d23a6e8a501a2227f7b4355a
RealTong <i@wst.sh>
2023-04-04 19:58:36 +0800

    fix: 🐛 Fix dark mode invalid bug
    Fix dark mode invalid bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  const isBrowser = typeof window !== "undefined";
+  if (isBrowser) {
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+      const html = document.querySelector("html");
+      if (e.matches) {
+        html.classList.add("dark");
+      } else {
+        html.classList.remove("dark");
+      }
+    });
+  }
   return <Component {...pageProps} />
 }
