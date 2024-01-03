eb337d92541801015afdf5c029ba37e0a5f55957
RealTong <i@wst.sh>
2024-01-03 22:02:45 +0800

    fix(fix): fix activity
    fix activity

--- src/components/ContributionGraph.tsx
+++ src/components/ContributionGraph.tsx
@@ -5,13 +5,13 @@ import GitHubCalendar from "react-github-calendar";
 import {GitHubUsername} from "../utils/consts";
 
 function GetContributionGraph() {
-  const [graphSize, setGraphSize] = useState<'small' | 'medium' | 'large'>('medium')
+  const [graphSize, setGraphSize] = useState<'small' | 'medium' | 'large'>()
 
   useEffect(() => {
     function handleWindowsWidthChange() {
-      if (window.innerWidth < 450) {
+      if (window.innerWidth < 512) {
         setGraphSize('small')
-      } else if (window.innerWidth < 640) {
+      } else if (window.innerWidth < 672) {
         setGraphSize('medium')
       } else {
         setGraphSize('large')
@@ -41,15 +41,15 @@ function GetContributionGraph() {
         data = contributions.slice(-365)
         break;
       default:
-        // 最近 6 个月
-        data = contributions.slice(-180)
+        // 全部
+        data = contributions
         break;
     }
     return data
   },[graphSize])
 
   return (
-    <div className={'p-2'}>
+    <div className={'w-full flex justify-center p-2'}>
       <GitHubCalendar
         username={GitHubUsername}
         transformData={transformData}
--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-import MusicBar from './MusicBar'
 import Link from 'next/link'
 
 function NowPlaying({ song }) {
   return (
     <div className="mb-8 flex w-full flex-row items-center space-x-4">
-      {/*<MusicBar />*/}
       <div className={'i-simple-icons-spotify transform rotate-45'}></div>
       <div className="inline-flex flex-col justify-center  truncate">
         <Link
