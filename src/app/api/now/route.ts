a9988ac232694cd32919970d1da66cc8752dc339
RealTong <i@wst.sh>
2024-01-26 01:19:54 +0800

    fix(fix): fix api
    fix api

--- src/app/api/now/route.ts
+++ src/app/api/now/route.ts
@@ -1,7 +1,7 @@
 import {getNowPlayingSVG} from '@/lib/spotify'
 
 export const revalidate = 0
-export const GET = async (response: Response) => {
+export const GET = async (_request:Request,response: Response) => {
   const song = await getNowPlayingSVG({coverFormat: 'url'})
   response.headers.set('Cache-Control', `s-maxage=1`)
   response.headers.set('Content-Type', 'image/svg+xml');
